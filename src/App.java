d1d3a7a0727e4bb20bcc523d9f2066221f51529d
Esteban Hernandez <joehv33@gmail.com>
2024-10-02 12:24:56 -0500

    Actualizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        MetodoOrdenamiento ordenar = new MetodoOrdenamiento();
+
+        int[] arreglo = {3,5,1,2,4};
+
+        System.out.println("El arreglo nuevo es: ");
+
+        int[] arreglo2 = ordenar.sort(arreglo,false);
+        ordenar.printArreglo(arreglo2);
+
     }
 }
---
+++ src/MetodoOrdenamiento.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class MetodoOrdenamiento {
+    
+    public int[] sort(int[] arreglo, boolean logs){
+        if (logs){
+            for (int i = 1; i < arreglo.length; i++){
+                System.out.println("El paso es: " + i);
+            int aux = arreglo[i];
+            int j = i - 1;     
+            System.out.println("\ti = " + i + "  j = " + j + "  [i] = " + arreglo[i] + "  [j] = " + arreglo[j]);       
+            while (j >= 0 && arreglo[j] > aux) {
+                System.out.println("\t\tComparamos " + aux + " con " + arreglo[j]);
+                arreglo[j + 1] = arreglo[j];
+                j--;
+                System.out.println("\t\t--------" + Arrays.toString(arreglo));
+            }
+            arreglo[j + 1] = aux;
+            System.out.println("\t--------" + Arrays.toString(arreglo));
+        }
+    } else {
+        for (int i = 1; i < arreglo.length; i++){
+            int aux = arreglo[i];
+            int j = i - 1;           
+            while (j >= 0 && arreglo[j] > aux) {
+                arreglo[j + 1] = arreglo[j];
+                j--;
+            }
+            arreglo[j + 1] = aux;
+        }
+    }
+    return arreglo;
+}
+
+    public void printArreglo(int[] arreglo){
+        for (int elemento : arreglo){
+            System.out.print(elemento + " ");
+        }
+    }
+}
